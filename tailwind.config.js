80ca9801143a0f68b1f1f6652ad28961aa8382c6
Phats Van <vanphat2k1@gmail.com>
2024-07-14 23:51:02 +0700

    Add freatured mugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,13 +17,19 @@ module.exports = {
         slieDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0%)' },
+        },
+        fadeIn: {
+          from: {opacity:0}, to:{opacity:1},
         }
       },
       animation:{
         slieDown: 'slieDown 0.4s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out',
       },
       backgroundImage:{
-        'slider-bg':'url("../image/bgslider.jpg")',
+        'slider-bg':"url('../image/bgslider.jpg')",
+        'fm1': "url('./image/fm1.jpg)",
+        'fm2': "url('./image/fm2.jpg')"
       }
     },
   },
